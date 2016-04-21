cd9e6ab290253bd12790c10c6c8317f225ea9dab
jitendra-1217 <jitendraojha1217@gmail.com>
2016-04-22 00:43:04 +0530

    Testing python package on openshift.

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,10 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+import config
+app.config.from_object(config)
+
+from flask.ext.basicauth import BasicAuth
+basic_auth = BasicAuth(app)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/secret")
+@basic_auth.required
+def secret():
+    return "Secret message!"
 
 if __name__ == "__main__":
     app.run()
